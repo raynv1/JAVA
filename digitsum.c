52651e02dd7a15612b99077f67ebcfdd077d28d5
raynv1 <54934980+raynv1@users.noreply.github.com>
2020-06-01 23:02:20 +0530

    Add files via upload
    Objective 
    In order to get the last digit of a number, we use modulo operator \%. When the number is modulo divided by 10 we get the last digit. 
    To find first digit of a number we divide the given number by 
    
    until number is greater than 
    
    . At the end we are left with the first digit. 
    Task
    In this challenge, you have to input a five digit number and print the sum of digits of the number. 
    Input Format
    The input contains a single five digit number, 
    
    .
    Constraints
    
    
    
    
    
    Output Format
    Print the sum of the digits of the five digit number.
    Sample Input 0
    10564
    Sample Output 0
    16

---
+++ digitsum.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+	
+    int n,r,sum;
+    scanf("%d", &n);
+    //Complete the code to calculate the sum of the five digits on n.
+    while(n>0)
+    {
+        r=n%10;
+        sum+=r;
+        n=n/10;
+        
+    }
+    printf("%d",sum);
+    return 0;
+}
