84121064df1e357f867e966884ac1081a094f231
raynv1 <54934980+raynv1@users.noreply.github.com>
2020-06-19 16:39:21 +0530

    Create bigintegerexample.java
    In this problem, you have to add and multiply huge numbers! These numbers are so big that you can't contain them in any ordinary data types like a long integer.
    
    Use the power of Java's BigInteger class and solve this problem.
    
    Input Format
    
    There will be two lines containing two numbers,  and .
    
    Constraints
    
     and  are non-negative integers and can have maximum  digits.
    
    Output Format
    
    Output two lines. The first line should contain , and the second line should contain . Don't print any leading zeros.
    
    Sample Input
    
    1234
    20
    Sample Output
    
    1254
    24680

---
+++ bigintegerexample.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.math.BigInteger; 
+
+public class bigintegerexample {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        BigInteger a=sc.nextBigInteger();
+        BigInteger b=sc.nextBigInteger();
+         BigInteger c,d;
+c=a.add(b);
+d=a.multiply(b);
+    System.out.println(c);
+     System.out.println(d);
+       
+    }
+}
