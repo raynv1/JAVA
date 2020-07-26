691279d53a0a200809824b106ef26458b5fb7ac5
raynv1 <54934980+raynv1@users.noreply.github.com>
2020-07-26 06:26:47 +0530

    Add files via upload
    hashset

---
+++ hashset1.java
@@ -0,0 +1,18 @@
+package hello;
+import java.util.*;
+public class hashset1 {
+
+	public static void main(String[] args) 
+		// TODO Auto-generated method stub
+		HashSet l=new HashSet();
+		l.add("B");
+		l.add("C");
+		l.add("D");
+		l.add("Z");
+		l.add(null);
+		System.out.println(l.add("Z"));
+		System.out.println(l);
+		
+	}
+
+}
