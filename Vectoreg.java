97169b45d98ebb16c501d81a24460760f82fe75d
raynv1 <54934980+raynv1@users.noreply.github.com>
2020-07-27 10:46:42 +0530

    Add files via upload
    example

---
+++ Vectoreg.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Vectoreg
+{
+public static void main(String args[])
+{
+int i;
+Vector V=new Vector();
+System.out.println(V.capacity());
+
+System.out.println(V.capacity());
+V.addElement("A");
+System.out.println(V.capacity());
+System.out.println(V);
+}}
