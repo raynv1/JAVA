523ebf43e7d4d673106a815ce0c318e7f0503e49
raynv1 <54934980+raynv1@users.noreply.github.com>
2020-10-23 06:04:08 +0530

    Create StringCharacter.java
    char

---
+++ StringCharacter.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+
+    // create string using the string literal
+    String greet = "Hello! World";
+    System.out.println("The string is: " + greet);
+
+    // returns the character at 3
+    System.out.println("The character at 3: " + greet.charAt(3));
+
+    // returns the character at 7
+    System.out.println("The character at 7: " + greet.charAt(7));
+  }
+}
