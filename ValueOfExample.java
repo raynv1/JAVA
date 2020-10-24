70aae878ea9b4a660526b51e42cc2d53c7f77c77
raynv1 <54934980+raynv1@users.noreply.github.com>
2020-10-24 06:05:47 +0530

    Create ValueOfExample.java
    value of

---
+++ ValueOfExample.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+    char c = 'J';
+    char ch[] = {'J', 'a', 'v', 'a'};
+
+    // convert char to string
+    System.out.println(String.valueOf(c));  // "J"
+
+    // convert char array to string
+    System.out.println(String.valueOf(ch));  // "Java"
+  }
+}
