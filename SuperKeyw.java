a155e73a359dc35ab139cb0b76a27c96957c2254
raynv1 <54934980+raynv1@users.noreply.github.com>
2020-11-29 06:32:58 +0530

    Create SuperKeyw.java
    Super

---
+++ SuperKeyw.java
@@ -0,0 +1,29 @@
+public void display(){
+    System.out.println("I am an animal");
+  }
+}
+
+class Dog extends Animal {
+
+  // overriding method
+  @Override
+  public void display(){
+    System.out.println("I am a dog");
+  }
+
+  public void printMessage(){
+
+    // this calls overriding method
+    display();
+
+    // this calls overridden method
+    super.display();
+  }
+}
+
+class Main {
+  public static void main(String[] args) {
+    Dog dog1 = new Dog();
+    dog1.printMessage();
+  }
+}
